ead5adf7b66672498bc2c848afaa8604ffec3157
Usmajabeen11 <80537322+Usmajabeen11@users.noreply.github.com>
2021-03-22 16:34:19 +0500

    Add files via upload

---
+++ Euclid.java
@@ -0,0 +1,14 @@
+public Class Euclid {
+public static int gcd(int p, int q){
+ if ( q == 0) return p;
+    else return gcd( q, p% q);
+ }
+public static void main (String[] args) {
+   int p = Integer.parseInt(args[0]);
+    int q = Integer.parseInt(args[1]);  
+    int d = gcd(p, q);
+     int d2 = gcd2(p, q);
+    System.out.println("gcd (" + p + ", " + q +") = " + d); 
+    System.out.println("gcd (" + p + ", " + q +") = " + d2);
+ }
+} 
